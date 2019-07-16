febf930bc5413fcf140d618d559c850871a14638
Binu <kbinu42@gmail.com>
2019-07-16 20:15:41 +0530

    google auth implimented

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -7,7 +15,10 @@ app.get('/', (req, res) => {
     res.send('it works');
 })
 
-port = process.env.PORT || 4000;
+// use routes
+app.use('/auth', auth);
+
+port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
 })
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin-Binu:'+ process.env.mongoDBPassword +'@cluster0-9npsv.mongodb.net/storyDB',
+    googleClientID: process.env.google_Client_ID,
+    googleClientSecret: process.env.google_Client_Secret
+}
